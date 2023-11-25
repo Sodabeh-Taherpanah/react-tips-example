a7f81613f7c69ec2b2b09f053773f8b7e641083e
sooodi <sudytaher@gmail.com>
2023-11-25 20:36:41 +0330

    Create MountedComponent.js

---
+++ MountedComponent.js
@@ -0,0 +1,29 @@
+import { useCallback, useEffect, useRef } from "react";
+
+export function useIsMounted() {
+  const isMountedRef = useRef(true);
+  const isMounted = useCallback(() => isMountedRef.current, []);
+
+  useEffect(() => {
+    return () => void (isMountedRef.current = false);
+  }, []);
+
+  return isMounted;
+}
+//usage 
+function ComponentA() {
+  const [data, setData] = React.useState()
+  const isMounted = useIsMounted()
+
+  React.useEffect(() => {
+    //for example we can fetch().then((data) => {
+      // at this point the component may already have been removed from the tree
+      // so we need to check first before updating the component state
+      if (isMounted()) {
+        setData(data)
+     // }
+    })
+  }, [...])
+
+  return ( <div> componenA </div>)
+}

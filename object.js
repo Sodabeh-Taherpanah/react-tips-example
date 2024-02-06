611c3151973dfd657dce57c8b128445897c0ee99
sooodi <sudytaher@gmail.com>
2024-02-06 09:21:03 +0330

    Update object.js

--- object.js
+++ object.js
@@ -1,1 +1,24 @@
+obj={
+  "user1":"ali",
+  "user2":"mina"
+}
+object.keys(obj).forEach(e=>{
+  console.log(key,obj(key))   ->>  user1  , ali   
+                                  user2 , mina
+})
 
+object.values(obj).forEach(e=>{
+  console.log(e)   ->>   ali   
+                        mina
+})
+
+object.entries(obj).forEach(e=>{
+  console.log(e)   ->>         [user1,ali]
+                               [user2 ,mina]
+})
+
+
+
+object.entries(obj).map(e=>{
+  return   e[1]+e[0].replace('user1',"me');   [alime, mina]   
+})

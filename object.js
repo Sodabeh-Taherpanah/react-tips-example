792dbf2299e556055ba732332400fac8e27f54f7
sooodi <sudytaher@gmail.com>
2024-04-21 16:24:20 +0330

    Update object.js

--- object.js
+++ object.js
@@ -18,7 +18,6 @@ object.entries(obj).forEach(e=>{
 })
 
 
-
 object.entries(obj).map(e=>{
   return   e[1]+e[0].replace('user1',"me');   [alime, mina]   
 })
@@ -27,3 +26,12 @@ for(item in obj)   // enumerating  for objects
   {
     console.log(item)
   }
+const obj2={user:{age:23,height:160}} 
+obj2?.user?.age  ? is optional chaining operator
+
+nullish coalescing operator ??    
+//The nullish coalescing (??) operator is a logical operator that returns its right-hand side operand when its left-hand side operand is null or undefined, 
+const foo = null ?? 'default string';
+console.log(foo);   //'default string'
+const baz = 0 ?? 42;
+console.log(baz);  //  0
